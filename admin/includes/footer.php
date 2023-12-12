e1f6f57f43419cf1db7fd9f962140423daecb3db
Himanshi Painuly <146033389+Peacemaker1999@users.noreply.github.com>
2023-12-12 03:54:28 +0000

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,6 +2,6 @@
     <strong>YCRS &copy; 2020 .</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Yoga</b>Class Registration System
+      <b>Yoga</b>Class Registration System by Himanshi Painuly
     </div>
   </footer>
